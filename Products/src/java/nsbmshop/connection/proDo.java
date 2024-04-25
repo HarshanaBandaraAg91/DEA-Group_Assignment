8e658367ab0cb56f94d0a17af3185cc9d1a6751f
ThisangiRanasinghe <137670223+ThisangiRanasinghe@users.noreply.github.com>
2024-04-25 09:33:49 +0530

    Add files via upload

---
+++ Products/src/java/nsbmshop/connection/proDo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nsbmshop.connection;
+
+/**
+ *
+ * @author Thisangi Ranasinghe
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class proDo {
+    private static Connection connection = null;
+	public static Connection getConnection() throws ClassNotFoundException, SQLException{
+        if(connection == null){
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/nsbmShop","root","ASrt12@@");
+            System.out.print("connected");
+        }
+        return connection;
+    }
+    
+}
